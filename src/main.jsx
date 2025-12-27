bc523a5d7d337360cd282840d117d0f8864d3339
juandualibe <juandualibe@gmail.com>
2025-12-27 20:53:10 -0300

    Probando para versión instalable

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext'; // <-- 1. Importar el Provider
+import { AuthProvider } from './context/AuthContext';
+// IMPORTANTE: Importamos el registro manual de PWA
+import { registerSW } from 'virtual:pwa-register'; 
 import './index.css';
 import App from './App.jsx';
 
+// Activamos la PWA inmediatamente
+registerSW({ immediate: true });
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter> {/* <--- 2. Envolver todo con el Router */}
-      <AuthProvider> {/* <--- 3. Envolver App con el AuthProvider */}
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>,
 );
